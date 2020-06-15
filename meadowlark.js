2d465f470461871ba42fefafd150686962ae4947
Blaine137 <52352305+Blaine137@users.noreply.github.com>
2020-06-15 09:52:32 -0400

    ran into a error at end of chapter 9

--- meadowlark.js
+++ meadowlark.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
-const multiparty = require('multiparty')
 const cookieParser = require('cookie-parser')
-const { credentials } = require('./config')
+const expressSession = require('express-session')
+
+const credentials = require('./credentials')
 const handlers = require('./lib/handlers')
 const weatherMiddlware = require('./lib/middleware/weather')
-const expressSession = require('express-session')
 const flashMiddleware = require('./lib/middleware/flash')
 
 const app = express()
@@ -24,26 +24,22 @@ app.engine('handlebars', expressHandlebars({
 }))
 app.set('view engine', 'handlebars')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.use(cookieParser(credentials.cookieSecret))
 app.use(expressSession({
   resave: false,
   saveUninitialized: false,
   secret: credentials.cookieSecret,
 }))
 
-app.use(flashMiddleware)
-
-app.use(cookieParser(credentials.cookieSecret))
-
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-
 const port = process.env.PORT || 3000
-
-
 
 app.use(express.static(__dirname + '/public'))
 
 app.use(weatherMiddlware)
+app.use(flashMiddleware)
 
 app.get('/', handlers.home)
 
@@ -51,31 +47,7 @@ app.get('/', handlers.home)
 app.get('/newsletter-signup', handlers.newsletterSignup)
 app.post('/newsletter-signup/process', handlers.newsletterSignupProcess)
 app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou)
-
-// handlers for fetch/JSON form submission
-app.get('/newsletter', handlers.newsletter)
-app.post('/api/newsletter-signup', handlers.api.newsletterSignup)
-
-// vacation photo contest
-app.get('/contest/vacation-photo', handlers.vacationPhotoContest)
-app.get('/contest/vacation-photo-ajax', handlers.vacationPhotoContestAjax)
-app.post('/contest/vacation-photo/:year/:month', (req, res) => {
-  const form = new multiparty.Form()
-  form.parse(req, (err, fields, files) => {
-    if(err) return handlers.vacationPhotoContestProcessError(req, res, err.message)
-    console.log('got fields: ', fields)
-    console.log('and files: ', files)
-    handlers.vacationPhotoContestProcess(req, res, fields, files)
-  })
-})
-app.get('/contest/vacation-photo-thank-you', handlers.vacationPhotoContestProcessThankYou)
-app.post('/api/vacation-photo-contest/:year/:month', (req, res) => {
-  const form = new multiparty.Form()
-  form.parse(req, (err, fields, files) => {
-    if(err) return handlers.api.vacationPhotoContestError(req, res, err.message)
-    handlers.api.vacationPhotoContest(req, res, fields, files)
-  })
-})
+app.get('/newsletter-archive', handlers.newsletterSignupThankYou)
 
 app.use(handlers.notFound)
 app.use(handlers.serverError)
